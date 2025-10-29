8bfab492539d665635ac894c92c5a2347cdba747
Jonatan Hedborg <jonatan.hedborg@gmail.com>
2025-10-29 17:54:38 +0100

    Made distance sensor only run once per second

--- Garage_Opener_Logicer_R6/DistanceSensor.cpp
+++ Garage_Opener_Logicer_R6/DistanceSensor.cpp
@@ -1,17 +1,26 @@
 #include "DistanceSensor.h"
 #include "Arduino.h"
 
-DistanceSensor::DistanceSensor(int trigger, int sensor) : trigger_pin(trigger), sensor_pin(sensor) {}
+DistanceSensor::DistanceSensor(int trigger, int sensor) : trigger_pin(trigger), sensor_pin(sensor) {
+	time_of_last_measurement = 0;
+	cached_value = -1;
+}
 
 int DistanceSensor::get_distance() {
-	digitalWrite(trigger_pin, LOW);
-	delayMicroseconds(2);
+	if(millis() > time_of_last_measurement + TIME_BETWEEN_MEASUREMENTS || cached_value == -1) {
+		digitalWrite(trigger_pin, LOW);
+		delayMicroseconds(2);
 
-	digitalWrite(trigger_pin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigger_pin, LOW);
+		digitalWrite(trigger_pin, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(trigger_pin, LOW);
 
-	unsigned long duration = pulseIn(sensor_pin, HIGH);
+		unsigned long duration = pulseIn(sensor_pin, HIGH);
 
-	return duration * SOUND_VELOCITY/2;
+		cached_value = duration * SOUND_VELOCITY/2;
+
+		time_of_last_measurement = millis();
+	}
+
+	return cached_value;
 }
--- Garage_Opener_Logicer_R6/DistanceSensor.h
+++ Garage_Opener_Logicer_R6/DistanceSensor.h
@@ -8,9 +8,15 @@ public:
 
 private:
 	const float SOUND_VELOCITY = 0.034f;
+	const int TIME_BETWEEN_MEASUREMENTS = 1000;
+
 
 	int trigger_pin;
 	int sensor_pin;
+
+	int cached_value;
+
+	unsigned long time_of_last_measurement = 0;
 };
 
 #endif
